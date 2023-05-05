5cd963d43a60fe54b35c770afefd227c444bcf23
coolbele <64392071+coolbele@users.noreply.github.com>
2023-05-05 19:17:05 +0800

    Update diy-by-packages.sh

--- shell/diy-by-packages.sh
+++ shell/diy-by-packages.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages" >> ./feeds.conf.default
 git pull

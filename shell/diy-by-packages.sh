1a7ba078affaef474cf2eaaf0e4be0c96c0e215a
coolbele <64392071+coolbele@users.noreply.github.com>
2023-05-10 20:47:41 +0800

    Update diy-by-packages.sh

--- shell/diy-by-packages.sh
+++ shell/diy-by-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages" >> ./feeds.conf.default
+echo "src-git liuran001 https://github.com/liuran001/openwrt-packages" >> ./feeds.conf.default
 git pull
